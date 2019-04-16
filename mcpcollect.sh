1dad701407d1122d6f39063e19b107b056dc3be5
Jerry Keen <jkeen@io.lan1>
2019-04-15 21:07:54 -0500

    first review with Ryan

--- mcpcollect.sh
+++ mcpcollect.sh
@@ -4,11 +4,19 @@
 function usage(){
 	echo "    mcpcollector -c <nova|neutron|stacklight|ceph>"
 	echo ""
-	echo "    -c component"
-	echo "    -i instance id"
+	echo "    -c component "
+	echo "         <nova|neutron|stacklight|ceph|rabbitmq|cinder|contrail>"
+	echo "	  -h target hostname or IP"
+	echo "    -o ceph OSD"
+	echo "    -t timeframe yymmddhhmm-yymmddhhmm"
+	echo "    -i some ID"
+	echo "    -r rotated logs"
+	echo "		<Y|N>"
+	echo "	  "
 
 }
 
+# may use optarg
 
 while getopts "hc:n:e:" arg; do
   case $arg in
@@ -18,37 +26,76 @@ while getopts "hc:n:e:" arg; do
 done
 
 
+# Nova instances
+# OVS
+# Versions of all services
+# ceph pg
+# Service status
+# NTH : connectivity/network
+# Networking Gereral Health
+# Cinder ?
+# Heat Stack
+# Nova instance files : ls -al /var/lib/nova/instances/ (on a compute node)
+# Cinder  -- /var/lib/cinder/volumes/
 
 
 
 ## MCP Collector ##
 
-targetdir="/tmp/mcpcollector"
+targetdir="/tmp/mcpcollect"
+localtargetdir="/tmp/mcpcollect"
 
 [[ -d dir ]] || mkdir $targetdir
 
+if [ "$component" = "ceph" ]; then
+	### Ceph General ###
+	cephLogDir="/var/log/ceph"
+	cephOsdDir="/var/log/ceph"
+	declare -a Cmd ("ceph -s" "ceph health detail" "ceph --version")
+	declare -a Log ("ceph.log")
 
-### Ceph General ###
-cephLogDir="/var/log/ceph"
-cephOsdDir="/var/log/ceph"
-declare -a cephCmd ("ceph -s" "ceph health")
-declare -a cephLog ("ceph.log")
+elif [ "$component" = "cephosd" ]; then
+	### Ceph OSD ###
+	declare -a Log 
 
-### Ceph OSD ###
-declare -a cephOsdLog 
+elif [ "$component" = "nova" ]; then
+	### Nova ###
+	declare -a Cmd ("nova hypervisor-list" "nova list --fields name,networks,host --all-tenants")
+	declare -a Log ("/var/log/nova" "/var/log/nova/nova-api")
 
-### Nova ###
-declare -a cmd_nova ("nova hypervisor-list" "nova list --fields name,networks,host --all-tenants")
-declare -a logs_nova ("/var/log/nova")
-declare -a logs_api_nova ("/var/log/nova/nova-api")
-
-### Reclas Model ###
-declare -a reclassGet ("tar -zcvf `date '+%Y%m%d%H%M%S'`.tar.gz /var/salt/reclass $targetdir")
-
+elif [ "$component" = "reclass" ]; then
+	### Reclas Model ###
+	declare -a Cmd ("tar -zcvf `date '+%Y%m%d%H%M%S'`.tar.gz /var/salt/reclass $targetdir")
+fi
 
 
 ## Run Function ##
 
 # RunCommands <targethost> <command|log> <commandString|logString>
 
+fuction collectdata {
+	targethost=$1
+	commandorlog=$2 
+	if [ "$2" = "CMD" ]; then
+		echo "ssh to any host except cfg and execute command, if on salt commands run locally"
 
+	elif [ "$2" = "LOG" ]; then
+		echo "log"
+	fi
+}
+
+
+
+fuction pullresults {
+	echo "scp from targetdir on target host to $localtargetdir"
+
+}
+
+
+
+# a=(foo bar "foo 1" "bar two")  #create an array
+#b=("${a[@]}")                  #copy the array in another one 
+
+#for value in "${b[@]}" ; do    #print the new array 
+#echo "$value" 
+#done   
