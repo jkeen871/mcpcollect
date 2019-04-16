26815571a5a4e7b8794b81aa089d1ff29f659178
Jerry keen <jkeen@astro.lan1>
2019-04-16 09:42:37 -0500

    Optargs corrected

--- mcpcollect.sh
+++ mcpcollect.sh
@@ -18,13 +18,12 @@ function usage(){
 
 # may use optarg
 
-while getopts "hc:n:e:" arg; do
+while getopts "c:h:o:t:i:r:" arg; do
   case $arg in
-	  c);;
-    i);;
+	  c) component="$OPTARG";;
+	  h) targethost="$OPTARG";;
   esac
 done
-
 
 # Nova instances
 # OVS
@@ -45,53 +44,67 @@ done
 targetdir="/tmp/mcpcollect"
 localtargetdir="/tmp/mcpcollect"
 
-[[ -d dir ]] || mkdir $targetdir
-
-if [ "$component" = "ceph" ]; then
-	### Ceph General ###
-	cephLogDir="/var/log/ceph"
-	cephOsdDir="/var/log/ceph"
-	declare -a Cmd ("ceph -s" "ceph health detail" "ceph --version")
-	declare -a Log ("ceph.log")
-
-elif [ "$component" = "cephosd" ]; then
-	### Ceph OSD ###
-	declare -a Log 
-
-elif [ "$component" = "nova" ]; then
-	### Nova ###
-	declare -a Cmd ("nova hypervisor-list" "nova list --fields name,networks,host --all-tenants")
-	declare -a Log ("/var/log/nova" "/var/log/nova/nova-api")
-
-elif [ "$component" = "reclass" ]; then
-	### Reclas Model ###
-	declare -a Cmd ("tar -zcvf `date '+%Y%m%d%H%M%S'`.tar.gz /var/salt/reclass $targetdir")
+if [[ ! -e "$targetdir" ]]; then
+	mkdir -p $targetdir
+	### Check for error creating directory
 fi
+
+case $component in 
+	ceph) ### Ceph General ###
+		cephLogDir="/var/log/ceph"
+		cephOsdDir="/var/log/ceph"
+		declare -a Cmd=("ceph -s" "ceph health detail" "ceph --version")
+		declare -a Log=("ceph.log")
+		;;
+
+	cephosd) ### Ceph OSD ###
+		declare -a Log=("")
+		;;
+	nova) 	### Nova ###
+		declare -a Cmd=("nova hypervisor-list" "nova list --fields name,networks,host --all-tenants")
+		declare -a Log=("/var/log/nova" "/var/log/nova/nova-api")
+		;;
+	reclass) ### Reclas Model ###
+		declare -a Cmd=("tar -zcvf `date '+%Y%m%d%H%M%S'`.tar.gz /var/salt/reclass $targetdir")
+		;;
+	*) 
+		usage
+		;;
+esac
+
+
 
 
 ## Run Function ##
 
 # RunCommands <targethost> <command|log> <commandString|logString>
 
-fuction collectdata {
+function collectdata {
 	targethost=$1
 	commandorlog=$2 
-	if [ "$2" = "CMD" ]; then
+	if [ "$2"="CMD" ]; then
 		echo "ssh to any host except cfg and execute command, if on salt commands run locally"
 
-	elif [ "$2" = "LOG" ]; then
+	elif [ "$2"="LOG" ]; then
 		echo "log"
 	fi
 }
 
 
 
-fuction pullresults {
+function pullresults {
 	echo "scp from targetdir on target host to $localtargetdir"
 
 }
 
-
+echo ""
+printf '%s:     %s\n' "Target host" "$targethost"
+printf '%s:     %s\n' "Component" "$component"
+printf '%s\n' "Executing:"
+printf '	%s\n' "${Cmd[@]}"
+printf '%s\n' "Collecting logs:"
+printf '	%s\n' "${Log[@]}"
+echo ""
 
 # a=(foo bar "foo 1" "bar two")  #create an array
 #b=("${a[@]}")                  #copy the array in another one 
