a24869c16eea4f7bfea2613f370c8acb08e316f6
Jerry Keen <jkeen871@gmail.com>
2019-04-18 11:49:24 -0500

    complete all svc,log,cmd,cfg colletion

--- mcpcollect.sh
+++ mcpcollect.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+keystonercfile="/root/keystonercv3"
+remotetargetdir="/tmp/mcpcollect"
+localtargetdir="/tmp/mcpcollect"
+
+sshCmd=". $keystonercfile;"
 
 
 function usage(){
 	echo "    mcpcollector -c <nova|neutron|stacklight|ceph>"
 	echo ""
+	echo "    -C confighost"
 	echo "    -c component "
 	echo "         <nova|neutron|stacklight|ceph|rabbitmq|cinder|contrail>"
 	echo "	  -h target hostname or IP"
@@ -18,10 +24,11 @@ function usage(){
 
 # may use optarg
 
-while getopts "c:h:o:t:i:r:" arg; do
+while getopts "C:c:h:o:t:i:r:" arg; do
   case $arg in
 	  c) component="$OPTARG";;
 	  h) targethost="$OPTARG";;
+	  C) confighost="$OPTARG";;
   esac
 done
 
@@ -51,68 +58,68 @@ fi
 
 case $component in 
 	keystone) 
-		declare -a Log=(	"/var/log/keystone/*.log" \
+		declare -a Log=(	"/var/log/keystone/*.log" 			\
 				)
-		declare -a Cfg=(	"/etc/keystone/keystone.conf" \
+		declare -a Cfg=(	"/etc/keystone/keystone.conf" 			\
 				)
-		declare -a Svc=(	"apache2.service" \
+		declare -a Svc=(	"apache2.service" 				\
 				)
-		declare -a Cmd=(	"none"	\
+		declare -a Cmd=(	""						\
 				)
 	;;
 	horizon)
-		declare -a Log=(	"/var/log/horizon/*.log" \
+		declare -a Log=(	"/var/log/horizon/*.log" 			\
 				)
-		declare -a Cfg=(	"apache2.service" 	\
+		declare -a Svc=(	"apache2.service" 				\
 				)
-		declare -a Cmd=(	"netstat -nltp | egrep ':80|:443'" \
+		declare -a Cmd=(	"netstat -nltp | egrep ':80|:443'" 		\
 				)
-		declare -a Cfg=(	"/etc/apache/*" \
+		declare -a Cfg=(	"/etc/apache/*" 				\
+				)
 	;;	
 	neutron)
-		declare -a Log=(	"/var/log/neutron/openvswitch-agent.log" \
-					"/var/log/neutron/openvswitch-l3-agent.log" \
+		declare -a Log=(	"/var/log/neutron/*" 				\
 				)
-		declare -a Cfg=(	"/etc/neutron/plugins/ml2/ml2_conf.ini" \
-					"/etc/neutron/plugins/ml2/openvswitch_agent.ini" \
+		declare -a Cfg=(	"/etc/neutron/plugins/ml2/ml2_conf.ini" 	\
+					"/etc/neutron/plugins/ml2/openvswitch_agent.ini"\
 				)
-		declare -a Svc=(	"neutron-openvswitch-agent.service"   \
+		declare -a Svc=(	"neutron-openvswitch-agent.service"   		\
 				)
-		declare -a Cmd=(	"neutron agent-list"\
+		declare -a Cmd=(	"neutron agent-list"				\
 				)
 	;;
 	cinder)
-		declare -a Log=(	"/var/log/cinder/cinder.log"\
+		declare -a Log=(	"/var/log/cinder/cinder.log"			\
 				)
-		declare -a Cfg=(	"/etc/cinder/*" \
+		declare -a Cfg=(	"/etc/cinder/*" 				\
 				)
-		declare -a Svc=(	"cinder-scheduler.service" \
-					"cinder-volume.service" \
+		declare -a Svc=(	"cinder-scheduler.service" 			\
+					"cinder-volume.service" 			\
 				)
-		ceclare -a Cmd=(	"ls /var/lib/cinder/volumes" \
+		declare -a Cmd=(	"ls /var/lib/cinder/volumes" 			\
 				)
 	;;
 	ceph) 
 		### Ceph General ###
 		cephLogDir="/var/log/ceph"
 		cephOsdDir="/var/log/ceph"
-		declare -a Cmd=(	"ceph -s" \ 
-					"ceph health detail" \
-				       	"ceph --version" \
-					"ceph df" \
-					"ceph pg dump" \
-					"ceph osd tree" \
+		declare -a Cmd=(	"ceph -s"					\
+					"ceph health detail" 				\
+				       	"ceph --version" 				\
+					"ceph df" 					\
+					"ceph pg dump" 					\
+					"ceph osd tree" 				\
 				)
-		declare -a Log=(	"ceph.log"\	
+		declare -a Log=(	"ceph.log"					\	
 				)
 	;;
 	cephallnodes) 
 		### Run this on all ceph nodes"
-		declare -a Cmd=(	"service ceph status" \ 
+		declare -a Cmd=(	"service ceph status" 				\ 
 				)
-		declare -a Log=(	"none"\
+		declare -a Log=(	"none"						\
 				) 
-		declare -a Cfg=(	"/etc/ceph/ceph.conf"\
+		declare -a Cfg=(	"/etc/ceph/ceph.conf"				\
 				)
 	;;
 
@@ -122,21 +129,18 @@ case $component in
 	;;
 	nova) 	
 		### Nova ###
-		declare -a Cmd=(	"nova hypervisor-list" \ 
+		declare -a Cmd=(	"nova hypervisor-list"				\
 					"nova list --fields name,networks,host --all-tenants" \
 				)
-		declare -a Log=(	"/var/log/nova/nova-conductor.log" \
-		       			"/var/log/nova/nova-api" \
-					"/var/log/nova-scheduler.log" \
-					"/var/log/nova/nova-placement-api.log" \
-					"/var/log/libvirt" \
+		declare -a Log=(	"/var/log/nova/*.log" 				\
+					"/var/log/libvirt/*.log" 				\
 				)
-		declare -a Svc=(	"nova-api.service" \
-					"nova-conductor.service" \
-					"nova-scheduler.service" \
+		declare -a Svc=(	"nova-api.service" 				\
+					"nova-conductor.service" 			\
+					"nova-scheduler.service" 			\
 				)
 
-		declare -a Cfg=(	"/etc/nova/nova.conf" \
+		declare -a Cfg=(	"/etc/nova/*" 				\
 				)
 
 	;;
@@ -188,9 +192,86 @@ printf '        %s\n' "${Cfg[@]}"
 
 echo ""
 
-# a=(foo bar "foo 1" "bar two")  #create an array
-#b=("${a[@]}")                  #copy the array in another one 
 
-#for value in "${b[@]}" ; do    #print the new array 
-#echo "$value" 
-#done   
+function collect {
+        collectTarget=$1
+        collectName=$2
+
+        ############ Collect files #########
+        # TODO:
+        # *** Clean up remote-target dir
+
+        sshCmd='sudo salt "*'$targethost'*" cmd.run "mkdir -p '$remotetargetdir';tar cvzf '$remotetargetdir'/'$component'-'$collectName'.tar.gz '$collectTarget'";scp -r '$targethost':'$remotetargetdir'/* '$remotetargetdir'/'
+#       echo $sshCmd
+        ssh $confighost $sshCmd
+}
+
+function cleantargethost {
+        sshCmd='sudo salt "*'$targethost'*" cmd.run "rm -fR '$remotetargetdir'"'
+        ssh $confighost $sshCmd
+#       echo $sshCmd
+
+}
+
+function transferResults {
+        sshCmd='mkdir -p '$remotetargetdir';scp -r '$targethost':'$remotetargetdir'/* '$remotetargetdir
+        ssh $confighost $sshCmd
+#       echo $sshCmd
+}
+
+
+
+
+
+
+
+
+
+############## Execute Commands ################
+
+lenCmd=${#Cmd[@]}
+countCmd=1
+for (( i=0; i<${lenCmd}; i++ ));
+do
+
+	sshCmd+="echo '=';echo '==== ${Cmd[$i]}====';echo '=';${Cmd[$i]}"
+	if [ $countCmd -lt $lenCmd ]; then
+		sshCmd+=";"
+	fi
+#	echo $sshCmd
+	((countCmd++))
+
+done
+sshCmd='mkdir -p '$remotetargetdir';date > '$remotetargetdir'/'$component'; sudo salt "*'$targethost'*" cmd.run "'$sshCmd'" >> '$remotetargetdir'/'$component'-cmd'
+ssh $confighost $sshCmd
+
+
+#### Check Services ####
+sshCmd=""
+lenSvc=${#Svc[@]}
+countSvc=1
+for (( i=0; i<${lenSvc}; i++ ));
+do
+
+        sshCmd+="echo '=';echo '==== ${Svc[$i]}====';echo '=';systemctl status ${Svc[$i]}"
+        if [ $countSvc -lt $lenSvc ]; then
+                sshCmd+=";"
+        fi
+#        echo $sshCmd
+        ((countSvc++))
+
+done
+sshCmd='sudo salt "*'$targethost'*" cmd.run "'$sshCmd'" >> '$remotetargetdir'/'$component'-svc'
+echo $sshCmd
+#>i> '$remotetargetdir'/'$component
+ssh $confighost $sshCmd
+
+
+
+
+#### Collect data ####
+
+collect $Log "Logs"
+collect $Cfg "Confs"
+transferResults
+cleantargethost
